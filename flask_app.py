e7dff299891d10b8718ee14c74cb8b156b31629e
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-04-25 16:33:33 +0530

    summer season , mango very nice

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template ,send_from_directory
+from flask import Flask, render_template, send_from_directory
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
 
+app = Flask(__name__, static_url_path='/static')
 
-app = Flask(__name__,static_url_path='/static')
+# Load environment variables from .env file
+load_dotenv()
+
+# Configure MongoDB URI from environment variables
+app.config["MONGO_URI"] = os.getenv("MONGODB_URI")
+
+# Initialize PyMongo with the Flask app
+mongo = PyMongo(app)
 
 # Route for serving index.html
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Example: Fetch data from MongoDB and pass it to the template
+    data_from_mongodb = mongo.db.collection_name.find()
+    return render_template('index.html', data=data_from_mongodb)
 
 # Route for serving login.html
 @app.route('/login')
@@ -18,13 +31,15 @@ def login():
 def register():
     return render_template('register.html')
 
+# Route for serving config.json
 @app.route('/config.json')
 def get_config():
     return send_from_directory(app.static_folder, 'config.json')
 
+# Route for serving arc-sw.js
 @app.route('/arc-sw.js')
 def serve_js():
-    return send_file('arc-sw.js')
+    return send_from_directory(app.static_folder, 'arc-sw.js')
 
 if __name__ == '__main__':
     app.run(debug=True)
